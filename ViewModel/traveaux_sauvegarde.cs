f053b718756cc184d6d37afce8bf21e2016e80ee
sabineenibas <sabinefeddag@outlook.com>
2023-12-03 18:13:34 +0100

    [UPDATE]Travaux_sauvegarde

--- ViewModel/traveaux_sauvegarde.cs
+++ ViewModel/traveaux_sauvegarde.cs
@@ -52,8 +52,50 @@ namespace EasySaveG6.ViewModel
             this.type = type;
         }
 
+
+
+        // Convert the current backup to JSON format
+        public string travaux_sauvegardeToJSON()
+        {
+            var f = System.IO.File.Exists(@"..\..\..\Save\travaux_sauvegarde.txt");
+            if (f)
+            {
+                var toJSONN = new List<travaux_sauvegarde> {
+                    new travaux_sauvegarde{
+                        backupName = backupName,
+                        type = type,
+                        sourcePath = sourcePath,
+                        destinationPath = destinationPath,
+                        timestamp = timestamp,
+                    }
+                };
+                // Serialize the backup to JSON format
+                sauvegarde = JsonConvert.SerializeObject(toJSONN);
+            }
+
+            return sauvegarde;
+        }
+
+        // Display the list of backup tasks
+        public List<travaux_sauvegarde> displayBackups()
+        {
+            // Create the file if it doesn't exist
+            if (!System.IO.File.Exists(@"..\..\..\Save\travaux_sauvegarde.txt"))
+            {
+                System.IO.File.Create(@"..\..\..\Save\travaux_sauvegarde.txt");
+            }
+            // Read the saved backup tasks from file
+            string fRead = System.IO.File.ReadAllText(@"..\..\..\Save\travaux_sauvegarde.txt");
+            var workList = JsonConvert.DeserializeObject<List<travaux_sauvegarde>>(fRead);
+
+            return workList;
+        }
+
         
+
+
     }
+
 }
 
 
