8406c5bf07ac2740ac44ff2b2bfe33c6ed22a008
sabineenibas <sabinefeddag@outlook.com>
2023-12-03 18:43:37 +0100

    [UPDATE]Travaux_sauvegarde/ execute a Single Backup Task, multiple Backup Tasks, Get the Number of Backup Tasks

--- ViewModel/traveaux_sauvegarde.cs
+++ ViewModel/traveaux_sauvegarde.cs
@@ -52,8 +52,6 @@ namespace EasySaveG6.ViewModel
             this.type = type;
         }
 
-
-
         // Convert the current backup to JSON format
         public string travaux_sauvegardeToJSON()
         {
@@ -91,7 +89,99 @@ namespace EasySaveG6.ViewModel
             return workList;
         }
 
-        
+
+        // Execute a single backup task based on the provided name
+        public void executeSave(int name)
+        {
+            // Initialize fileT and status objects
+            EasySaveG6.Model.File fileT = new Status(@"..\..\..\Save\traveaux_sauvegarde.txt");
+            Status status = new Status(backupName, sourcePath, destinationPath, type);
+
+            // Check if the file containing backup tasks exists
+            var f = System.IO.File.Exists(@"..\..\..\Save\travaux_sauvegarde.txt");
+            if (f)
+            {
+                // Read the saved backup tasks and status from file
+                string fRead = System.IO.File.ReadAllText(@"..\..\..\Save\travaux_sauvegarde.txt");
+                var workList = JsonConvert.DeserializeObject<List<travaux_sauvegarde>>(fRead);
+
+                string fReadStatus = System.IO.File.ReadAllText(@"..\..\..\Save\Status.txt");
+                var workListStatus = JsonConvert.DeserializeObject<List<Status>>(fReadStatus);
+
+                // Create a Backup object based on the specified backup task
+                Backup b = new Backup(workList[name - 1].backupName, workList[name - 1].type, workList[name - 1].sourcePath, workList[name - 1].destinationPath);
+                b.backupUserChoice();
+
+                // Iterate through the status list to find and remove the executed backup task
+                for (var i = 0; i < workListStatus.Count; i++)
+                {
+                    if (workList[name - 1].backupName == workListStatus[i].backupName)
+                    {
+                        workList.RemoveAt(name - 1);
+                        var workListToString = JsonConvert.SerializeObject(workList);
+                        fileT.save(workListToString, @"..\..\..\Save\travaux_sauvegarde.txt");
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                // Create the file if it doesn't exist
+                System.IO.File.Create(@"..\..\..\Save\travaux_sauvegarde.txt");
+            }
+        }
+
+        // Execute multiple backup tasks based on their status
+        public void executeSaveMultiple()
+        {
+            // Initialize fileT and status objects
+            EasySaveG6.Model.File fileT = new Status(@"..\..\..\Save\traveaux_sauvegarde.txt");
+            Status status = new Status();
+
+            // Check if the file containing backup tasks exists
+            var f = System.IO.File.Exists(@"..\..\..\Save\travaux_sauvegarde.txt");
+
+            // Read the saved backup tasks and status from file
+            string fReadStatus = System.IO.File.ReadAllText(@"..\..\..\Save\Status.txt");
+            var workListStatus = JsonConvert.DeserializeObject<List<Status>>(fReadStatus);
+
+            string fRead = System.IO.File.ReadAllText(@"..\..\..\Save\travaux_sauvegarde.txt");
+            var workList = JsonConvert.DeserializeObject<List<travaux_sauvegarde>>(fRead);
+
+            // Iterate through the backup tasks and execute them based on their status
+            for (var i = 0; i < workListStatus.Count; i++)
+            {
+                Backup b = new Backup(workList[i].backupName, workList[i].type, workList[i].sourcePath, workList[i].destinationPath);
+                b.backupUserChoice();
+                if (workList[i].backupName == workListStatus[i].backupName && workListStatus[i].stateSave != "END")
+                {
+                    // Update the status of the backup task
+                    var workListToString = JsonConvert.SerializeObject(workList);
+                    fileT.save(workListToString, @"..\..\..\Save\travaux_sauvegarde.txt");
+                }
+            }
+        }
+
+        // Save the status of the current backup task
+        public void SaveStatus()
+        {
+            // Create a Status object and save its JSON representation to a file
+            Status status = new Status(backupName, sourcePath, destinationPath, type);
+            status.save(status.convertStatusToJSON(""), @"..\..\..\Save\Status.txt");
+        }
+
+        // Get the number of backup tasks
+        public int workNumber()
+        {
+            // Read the saved backup tasks from file and count them
+            string fRead = System.IO.File.ReadAllText(@"..\..\..\Save\travaux_sauvegarde.txt");
+            var ccount = fRead.Split("backupName").Length;
+            return ccount;
+        }
+
+
+
+
 
 
     }
